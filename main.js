ceeb280c4fe021932f278832823a149fba4997dd
koplo199 <85577251+koplo199@users.noreply.github.com>
2022-11-12 17:24:07 +0000

    Remove unnecessary semicolon
    Co-authored-by: Dawid Dziurla <dawidd0811@gmail.com>

--- main.js
+++ main.js
@@ -218,7 +218,7 @@ async function main() {
 
             core.info(`==> Downloading: ${artifact.name}.zip (${size})`)
 
-            let zip;
+            let zip
             try {
                 zip = await client.rest.actions.downloadArtifact({
                     owner: owner,
