80620a5d27ce0ae443b965134db88467fc607b43
Dawid Dziurla <dawidd0811@gmail.com>
2024-11-29 09:48:23 +0100

    node_modules: update

---
+++ node_modules/@azure/core-util/dist/react-native/httpMethods.d.ts
@@ -0,0 +1,6 @@
+/**
+ * @public
+ * Supported HTTP methods to use when making requests.
+ */
+export type HttpMethods = "GET" | "PUT" | "POST" | "DELETE" | "PATCH" | "HEAD" | "OPTIONS" | "TRACE";
+//# sourceMappingURL=httpMethods.d.ts.map
