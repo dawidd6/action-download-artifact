eba835a06ff6080191e3942d0c801f2753fa5af8
blu3 <blu3berry@disroot.org>
2025-01-08 18:13:59 +0300

    Remove node_modules

--- node_modules/@azure/core-util/dist/esm/httpMethods.d.ts
+++
@@ -1,6 +0,0 @@
-/**
- * @public
- * Supported HTTP methods to use when making requests.
- */
-export type HttpMethods = "GET" | "PUT" | "POST" | "DELETE" | "PATCH" | "HEAD" | "OPTIONS" | "TRACE";
-//# sourceMappingURL=httpMethods.d.ts.map
