883bc3da0e2f55ddd0a8fc8e99378551c1991a55
Dawid Dziurla <dawidd0811@gmail.com>
2026-01-30 15:26:47 +0100

    node_modules: update (#366)
    Co-authored-by: dawidd6 <9713907+dawidd6@users.noreply.github.com>

--- node_modules/@actions/github/node_modules/@octokit/request-error/dist-types/types.d.ts
+++
@@ -1,5 +0,0 @@
-import type { RequestOptions, OctokitResponse } from "@octokit/types";
-export interface RequestErrorOptions extends ErrorOptions {
-    response?: OctokitResponse<unknown> | undefined;
-    request: RequestOptions;
-}
